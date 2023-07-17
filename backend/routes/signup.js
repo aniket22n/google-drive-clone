e72aee7a081750836a9479e3023e69f02f07669f
aniket <ap8888678005@gmail.com>
2023-07-17 20:04:08 +0530

    modified signup route and added login route

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -1,32 +1,25 @@
-import { connectDB } from "../db/connection";
-import User from "../db/models/user";
-require("dotenv").config({ path: "../.env" });
+const User = require('../db/models/user');
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
-const jwt = require("jsonwebtoken");
 const secretKey = process.env.SECRET_KEY;
 
 async function signup(req, res) {
-  // connecting to database
-  await connectDB();
-
-  let { email, password } = req.body;
+  const { email, password } = req.body;
 
   // checking if an account with email already exits
-  let isUser = User.findOne({email})
+const isUser = await User.findOne({email})
   
   if (isUser) { 
     return res.status(409).json({message :"Account already exits"});
   }
-
   // save new user to database and returns jwt token
   let newUser = new User({
     email,
     password,
   });
 
-  newUser
-    .save()
-    .then(() => {
+  newUser.save().then(() => {
       let token = jwt.sign({ email }, secretKey, { expiresIn: "1hr" });
       res.status(200).json({ message: "User created successfully", token });
     })
